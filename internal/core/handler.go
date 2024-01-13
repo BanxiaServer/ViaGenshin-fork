496f0ece2d15d4a5bd4a4ff99b856ce70e969c22
亡灵暴龙大帝 <q200608233416@163.com>
2024-01-14 07:28:17 +0800

    更新 handler.go

--- internal/core/handler.go
+++ internal/core/handler.go
@@ -31,7 +31,6 @@ func LoadLuaShellCode() {
             logger.Error("not luac file, fileName: %v", fileName)
             continue
         }
-        name := split[0]
         data, err := os.ReadFile(LuaPathPrefix + fileName)
         if err != nil {
             logger.Error("read luac file error: %v, fileName: %v", err, fileName)
